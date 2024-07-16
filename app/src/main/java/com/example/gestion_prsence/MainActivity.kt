650c46403ed27a887e90e51362750c00f47abd94
Leslie <leslietsakeng5@gmail.com>
2024-07-16 15:28:04 +0100

    implemented auth sucessfully

--- app/src/main/java/com/example/gestion_prsence/MainActivity.kt
+++ app/src/main/java/com/example/gestion_prsence/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.gestion_prsence
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,8 +15,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gestion_prsence.ui.theme.Gestion_PrésenceTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +36,60 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+    public override fun onStart() {
+        super.onStart()
+        //createUserWithEmailPassCustom()
+        //signInWithEmailPassCustom()
+    }
+
+    private fun createUserWithEmailPassCustom(){
+        auth.createUserWithEmailAndPassword("laurel@gmail.com", "password")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    Toast.makeText(
+                        baseContext,
+                        "User created Successfully.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+    private fun signInWithEmailPassCustom(){
+        auth.signInWithEmailAndPassword("laurel@gmail.com", "password")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication succesfull. " + auth.currentUser?.email,
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
     }
 }
 
