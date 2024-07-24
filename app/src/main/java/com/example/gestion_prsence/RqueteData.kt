87cbd493e6386945397280c2283ace806c4550c8
Andelajean <ajeangael@gmail.com>
2024-07-24 15:55:11 +0200

    Ajout de la classe requete

---
+++ app/src/main/java/com/example/gestion_prsence/RqueteData.kt
@@ -0,0 +1,3 @@
+package com.example.gestion_prsence
+
+data class RqueteData(val email : String?= null, val motif : String? = null , val detail : String?=null , val heure : String? = null , val date: String?= null)
